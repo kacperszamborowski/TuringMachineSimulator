3548be44603400a8aad7ddb1d970c0d7724f3828
Kacper Szamborowski <kacperszamborowski@users.noreply.github.com>
2025-09-16 16:28:57 +0200

    Added polish/english errors for future

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,11 +5,21 @@ const messages = {
     header: "Symulator maszyny Turinga",
     addTape: "Dodaj taśmę",
     removeTape: "Usuń taśmę",
+    invalidRuleMissing: "Niepoprawna reguła (brak ->) w linii ",
+    invalidRuleTooMany: "Niepoprawna reguła (więcej niż 1 ->) w linii ",
+    invalidRuleInput: "Niepoprawna liczba symboli wejściowych w linii ",
+    invalidRuleOutput: "Niepoprawna liczba symboli wyjściowych lub ruchów w linii ",
+    invalidRuleMove: "Niepoprawny ruch w linii "
   },
   en: {
     header: "Turing machine simulator",
     addTape: "Add tape",
     removeTape: "Remove tape",
+    invalidRuleMissing: "Invalid rule (missing '->') in line ",
+    invalidRuleTooMany: "Invalid rule (more than 1 '->') in line ",
+    invalidRuleInput: "Invalid number of input symbols in line ",
+    invalidRuleOutput: "Invalid number of output symbols or moves in line ",
+    invalidRuleMove: "Invalid move in line "
   },
 };
 
